26e8626be4cacafb0be90cd2a535c01edad704ba
Shannon Lin <swlsheltie@gmail.com>
2024-06-03 12:59:36 -0400

    try new server code

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,9 @@
+require("dotenv").config();
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addShortcode("figmaAccessToken", function () {
-    return process.env.FIGMA_ACCESS_TOKEN;
-  });
   return {
     dir: {
       input: ".",
